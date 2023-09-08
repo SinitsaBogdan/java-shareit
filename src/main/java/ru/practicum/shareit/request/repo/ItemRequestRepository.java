ace9f0e3e1dc8914b944cee907ae85ba404edbc1
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 22:27:42 +0300

    update : request.repo - Создание базовых методов репозитория
    - ItemRequestRepository.java
    - ItemRequestRepositoryImpl.java

--- src/main/java/ru/practicum/shareit/request/repo/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repo/ItemRequestRepository.java
@@ -1,6 +1,21 @@
 package ru.practicum.shareit.request.repo;
 
-import ru.practicum.shareit.item.repo.ItemRepository;
 
-public class ItemRequestRepository implements ItemRepository {
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+public interface ItemRequestRepository {
+
+    List<ItemRequest> findAll();
+
+    ItemRequest findById(Long itemRequestId);
+
+    ItemRequest save(ItemRequest user);
+
+    ItemRequest update(ItemRequest user);
+
+    void deleteAll();
+
+    void deleteById(Long itemRequestId);
 }
--- src/main/java/ru/practicum/shareit/request/repo/ItemRequestRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/request/repo/ItemRequestRepositoryImpl.java
@@ -1,4 +1,38 @@
 package ru.practicum.shareit.request.repo;
 
-public interface ItemRequestRepositoryImpl {
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+public class ItemRequestRepositoryImpl implements ItemRequestRepository {
+
+    @Override
+    public List<ItemRequest> findAll() {
+        return null;
+    }
+
+    @Override
+    public ItemRequest findById(Long itemRequestId) {
+        return null;
+    }
+
+    @Override
+    public ItemRequest save(ItemRequest user) {
+        return null;
+    }
+
+    @Override
+    public ItemRequest update(ItemRequest user) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void deleteById(Long itemRequestId) {
+
+    }
 }

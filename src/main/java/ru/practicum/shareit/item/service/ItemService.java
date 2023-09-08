e5cad3e0102066e5dbfdd24834bd9fce77b3f332
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 18:43:59 +0300

    update : Обновление пакета item.service - Создание базовых методов сервиса
    - ItemService.java
    - ItemServiceImpl.java

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,4 +1,22 @@
 package ru.practicum.shareit.item.service;
 
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
+
 public interface ItemService {
+
+    List<ItemDto> getAll();
+
+    List<ItemDto> getAllByUser(long userId);
+
+    List<ItemDto> getBySearchText(String text);
+
+    ItemDto getById(long itemId);
+
+    ItemDto addToUser(long userId, ItemDto item);
+
+    ItemDto updateToUser(long userId, ItemDto item);
+
+    void deleteToUser(long userId, long itemId);
 }
--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,4 +1,43 @@
 package ru.practicum.shareit.item.service;
 
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
+
 public class ItemServiceImpl implements ItemService {
+
+    @Override
+    public List<ItemDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<ItemDto> getAllByUser(long userId) {
+        return null;
+    }
+
+    @Override
+    public List<ItemDto> getBySearchText(String text) {
+        return null;
+    }
+
+    @Override
+    public ItemDto getById(long itemId) {
+        return null;
+    }
+
+    @Override
+    public ItemDto addToUser(long userId, ItemDto item) {
+        return null;
+    }
+
+    @Override
+    public ItemDto updateToUser(long userId, ItemDto item) {
+        return null;
+    }
+
+    @Override
+    public void deleteToUser(long userId, long itemId) {
+
+    }
 }

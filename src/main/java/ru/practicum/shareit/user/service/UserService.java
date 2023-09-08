2622acab6ad51f2419ca6efc96128eb8e97be692
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 18:42:49 +0300

    update : Обновление пакета user.service - Создание базовых методов сервиса
    - UserService.java
    - UserServiceImpl.java

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,4 +1,18 @@
 package ru.practicum.shareit.user.service;
 
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<UserDto> getAll();
+
+    UserDto getById(long userId, long id);
+
+    UserDto add(long userId, UserDto user);
+
+    UserDto updateAll(long userId, UserDto user);
+
+    List<UserDto> delete(long userId);
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package ru.practicum.shareit.user.service;
 
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<UserDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserDto getById(long userId, long id) {
+        return null;
+    }
+
+    @Override
+    public UserDto add(long userId, UserDto user) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateAll(long userId, UserDto user) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> delete(long userId) {
+        return null;
+    }
 }

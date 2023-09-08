429a0e422bcc06fdfe89bf79d730ce37e5b87179
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 22:29:25 +0300

    update : item.repo - Создание базовых методов репозитория
    - ItemRepository.java
    - ItemRepositoryImpl.java

--- src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
@@ -1,4 +1,20 @@
 package ru.practicum.shareit.item.repo;
 
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
 public interface ItemRepository {
+
+    List<Item> findAll();
+
+    Item findById(Long itemId);
+
+    Item save(Item item);
+
+    Item update(Item item);
+
+    void deleteAll();
+
+    void deleteById(Long itemId);
 }
--- src/main/java/ru/practicum/shareit/item/repo/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repo/ItemRepositoryImpl.java
@@ -1,4 +1,42 @@
 package ru.practicum.shareit.item.repo;
 
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ItemRepositoryImpl implements ItemRepository {
+
+    private static final Map<Long, Item> data = new HashMap<>();
+
+    @Override
+    public List<Item> findAll() {
+        return (List<Item>) data.values();
+    }
+
+    @Override
+    public Item findById(Long itemId) {
+        return data.get(itemId);
+    }
+
+    @Override
+    public Item save(Item item) {
+        return data.put(item.getId(), item);
+    }
+
+    @Override
+    public Item update(Item item) {
+        return data.put(item.getId(), item);
+    }
+
+    @Override
+    public void deleteAll() {
+        data.clear();
+    }
+
+    @Override
+    public void deleteById(Long itemId) {
+        data.remove(itemId);
+    }
 }

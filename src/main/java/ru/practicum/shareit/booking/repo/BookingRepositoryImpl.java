4f9651a5fbce8c3fe3c1d0798e63edfab07d9778
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 22:28:31 +0300

    update : booking.repo - Создание базовых методов репозитория
    - BookingRepository.java
    - BookingRepositoryImpl.java

--- src/main/java/ru/practicum/shareit/booking/repo/BookingRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/booking/repo/BookingRepositoryImpl.java
@@ -1,4 +1,38 @@
 package ru.practicum.shareit.booking.repo;
 
+import ru.practicum.shareit.booking.model.Booking;
+
+import java.util.List;
+
 public class BookingRepositoryImpl implements BookingRepository {
+
+    @Override
+    public List<Booking> findAll() {
+        return null;
+    }
+
+    @Override
+    public Booking findById(Long bookingId) {
+        return null;
+    }
+
+    @Override
+    public Booking save(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public Booking update(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void deleteById(Long bookingId) {
+
+    }
 }

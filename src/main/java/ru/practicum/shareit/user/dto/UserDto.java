78130a8877240f74b4049456678e2b2d0f2325aa
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 21:58:34 +0300

    update : Обновление пакета user - Создание базовых моделей
    - UserDto.java
    - User.java

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * TODO Sprint add-item-requests.
  */
+
+@Data
+@Builder
 public class UserDto {
+
+    private String name, email;
 }

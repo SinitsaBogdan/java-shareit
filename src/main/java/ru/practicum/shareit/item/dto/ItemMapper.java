922dec5627aea766afc2e8f1d433de0b0dcd674a
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 19:53:04 +0300

    update : Обновление пакета item.dto - Создание Mapper класса
    - ItemMapper.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -1,14 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
+import org.jetbrains.annotations.NotNull;
 import ru.practicum.shareit.item.model.Item;
 
 public class ItemMapper {
 
-    public static ItemDto mapperItemToDto(Item item) {
-        return new ItemDto();
+    public static ItemDto mapperItemToDto(@NotNull Item item) {
+        return ItemDto.builder()
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
     }
 
     public static Item mapperItemDtoToItem(ItemDto itemDto) {
-        return new Item();
+        return Item.builder()
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .build();
     }
 }

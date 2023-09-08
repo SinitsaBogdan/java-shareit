75fc3e988b289d4b74caa9ea77d17e9ada0ffe74
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 22:10:32 +0300

    update : user.repo - Создание базовых методов репозитория
    - UserRepository.java
    - UserRepositoryImpl.java

--- src/main/java/ru/practicum/shareit/user/repo/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repo/UserRepository.java
@@ -1,4 +1,20 @@
 package ru.practicum.shareit.user.repo;
 
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    List<User> findAll();
+
+    User findById(Long userId);
+
+    User save(User user);
+
+    User update(User user);
+
+    void deleteAll();
+
+    void deleteById(Long userId);
 }
--- src/main/java/ru/practicum/shareit/user/repo/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repo/UserRepositoryImpl.java
@@ -1,4 +1,42 @@
 package ru.practicum.shareit.user.repo;
 
+import ru.practicum.shareit.user.model.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UserRepositoryImpl implements UserRepository {
+
+    private static final Map<Long, User> data = new HashMap<>();
+
+    @Override
+    public List<User> findAll() {
+        return (List<User>) data.values();
+    }
+
+    @Override
+    public User findById(Long userId) {
+        return data.get(userId);
+    }
+
+    @Override
+    public User save(User user) {
+        return data.put(user.getId(), user);
+    }
+
+    @Override
+    public User update(User user) {
+        return data.put(user.getId(), user);
+    }
+
+    @Override
+    public void deleteAll() {
+        data.clear();
+    }
+
+    @Override
+    public void deleteById(Long userId) {
+        data.remove(userId);
+    }
 }

f175c34d1102259258e452b1d976a615b3fee4c2
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 18:44:27 +0300

    update : Обновление пакета booking.service - Создание базовых методов сервиса
    - BookingService.java
    - BookingServiceImpl.java

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -1,4 +1,20 @@
 package ru.practicum.shareit.booking.service;
 
+import ru.practicum.shareit.booking.dto.BookingDto;
+
+import java.util.List;
+
 public interface BookingService {
+
+    List<BookingDto> getAll();
+
+    List<BookingDto> getAllByUser(long userId);
+
+    BookingDto getById(long bookingId);
+
+    BookingDto addToUser(long userId, BookingDto booking);
+
+    BookingDto updateToUser(long userId, BookingDto booking);
+
+    void deleteToUser(long userId, long bookingId);
 }
--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -1,4 +1,38 @@
 package ru.practicum.shareit.booking.service;
 
+import ru.practicum.shareit.booking.dto.BookingDto;
+
+import java.util.List;
+
 public class BookingServiceImpl implements BookingService {
+
+    @Override
+    public List<BookingDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<BookingDto> getAllByUser(long userId) {
+        return null;
+    }
+
+    @Override
+    public BookingDto getById(long bookingId) {
+        return null;
+    }
+
+    @Override
+    public BookingDto addToUser(long userId, BookingDto booking) {
+        return null;
+    }
+
+    @Override
+    public BookingDto updateToUser(long userId, BookingDto booking) {
+        return null;
+    }
+
+    @Override
+    public void deleteToUser(long userId, long bookingId) {
+
+    }
 }

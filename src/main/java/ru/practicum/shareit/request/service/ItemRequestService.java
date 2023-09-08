8048506a3dc26cd277e74879939658f583dc9952
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-08 18:43:33 +0300

    update : Обновление пакета request.service - Создание базовых методов сервиса
    - ItemRequestService.java
    - ItemRequestServiceImpl.java

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,4 +1,16 @@
 package ru.practicum.shareit.request.service;
 
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
+
 public interface ItemRequestService {
+
+    List<ItemRequestDto> getAll();
+
+    ItemRequestDto add(long userId, ItemRequestDto itemRequest);
+
+    ItemRequestDto update(long userId, ItemRequestDto itemRequest);
+
+    void deleteToUser(long userId, long itemRequestId);
 }
--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -1,4 +1,28 @@
 package ru.practicum.shareit.request.service;
 
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
+
 public class ItemRequestServiceImpl implements ItemRequestService {
+
+    @Override
+    public List<ItemRequestDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public ItemRequestDto add(long userId, ItemRequestDto itemRequest) {
+        return null;
+    }
+
+    @Override
+    public ItemRequestDto update(long userId, ItemRequestDto itemRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteToUser(long userId, long itemRequestId) {
+
+    }
 }

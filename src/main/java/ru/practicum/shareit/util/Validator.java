dffc7c9f1224d24a98f667bfd7c43f6b0ac9dd7b
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-09 03:09:30 +0300

    update : HelperClass - Validator

--- src/main/java/ru/practicum/shareit/util/Validator.java
+++ src/main/java/ru/practicum/shareit/util/Validator.java
@@ -2,6 +2,8 @@ package ru.practicum.shareit.util;
 
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.repo.ItemRepository;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.repo.UserRepository;
 import ru.practicum.shareit.util.exeptions.ShareitException;
 
 import static ru.practicum.shareit.util.exeptions.ErrorMessage.*;
@@ -17,5 +19,12 @@ public class Validator {
         if (item.getAvailable() == null) throw new ShareitException(ITEM_ERROR__VALID__AVAILABLE);
         if (item.getName() == null) throw new ShareitException(ITEM_ERROR__VALID__NAME);
         if (item.getDescription() == null) throw new ShareitException(ITEM_ERROR__VALID__DESCRIPTION);
+    }
+
+    public static void check(UserDto user, UserRepository repository) {
+        if (user.getName() == null || user.getName().isEmpty()) throw new ShareitException(USER_ERROR__VALID_EMPTY__NAME);
+        if (user.getEmail() == null || user.getEmail().isEmpty()) throw new ShareitException(USER_ERROR__VALID_EMPTY__EMAIL);
+        if (!user.getEmail().contains("@") || !user.getEmail().contains(".")) throw new ShareitException(USER_ERROR__VALID__EMAIL);
+        if (repository.checkEmailDuplicate(user.getEmail())) throw new ShareitException(USER_ERROR__VALID_DUPLICATE__EMAIL);
     }
 }

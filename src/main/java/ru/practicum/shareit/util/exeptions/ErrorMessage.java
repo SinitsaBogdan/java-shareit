80ead56ff91ace625fa850e64e1238815a2886fb
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-09 12:46:25 +0300

    update : ErrorMessage.java

--- src/main/java/ru/practicum/shareit/util/exeptions/ErrorMessage.java
+++ src/main/java/ru/practicum/shareit/util/exeptions/ErrorMessage.java
@@ -8,6 +8,7 @@ public enum ErrorMessage {
     GLOBAL_ERROR__NOT_HEADER_IN_REQUEST("Ошибка формата запроса!", "В запросе отсутствует заголовок X-Sharer-User-Id", 500),
 
     REPOSITORY_ERROR__ITEM__ID_NOT_IN_REPO__ID("Ошибка формата запроса!", "Указанный в заголовке X-Sharer-User-Id отсутствует в системе", 404),
+    REPOSITORY_ERROR__USER__ID_NOT_IN_REPO__ID("Ошибка формата запроса!", "Указанный id пользователя отсутствует в системе", 404),
 
     ITEM_ERROR__VALID__NAME("Ошибка формирования тела запроса!", "Поле name отсутствует или не в верном формате", 400),
     ITEM_ERROR__VALID__DESCRIPTION("Ошибка формирования тела запроса!", "Поле description отсутствует или не в верном формате", 400),

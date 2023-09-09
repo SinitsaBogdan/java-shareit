d60d1c32624122f59aee511c5a949e373418e2ff
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-09-09 03:18:59 +0300

    update : Validator.java

--- src/main/java/ru/practicum/shareit/util/Validator.java
+++ src/main/java/ru/practicum/shareit/util/Validator.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.util;
 
+import org.jetbrains.annotations.NotNull;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.repo.ItemRepository;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -15,13 +16,13 @@ public class Validator {
         if (!repository.checkId(id)) throw new ShareitException(REPOSITORY_ERROR__ITEM__ID_NOT_IN_REPO__ID);
     }
 
-    public static void check(ItemDto item) {
+    public static void check(@NotNull ItemDto item) {
         if (item.getAvailable() == null) throw new ShareitException(ITEM_ERROR__VALID__AVAILABLE);
         if (item.getName() == null) throw new ShareitException(ITEM_ERROR__VALID__NAME);
         if (item.getDescription() == null) throw new ShareitException(ITEM_ERROR__VALID__DESCRIPTION);
     }
 
-    public static void check(UserDto user, UserRepository repository) {
+    public static void check(@NotNull UserDto user, UserRepository repository) {
         if (user.getName() == null || user.getName().isEmpty()) throw new ShareitException(USER_ERROR__VALID_EMPTY__NAME);
         if (user.getEmail() == null || user.getEmail().isEmpty()) throw new ShareitException(USER_ERROR__VALID_EMPTY__EMAIL);
         if (!user.getEmail().contains("@") || !user.getEmail().contains(".")) throw new ShareitException(USER_ERROR__VALID__EMAIL);

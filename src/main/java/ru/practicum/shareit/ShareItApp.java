a19657e86d407e06880426244f445ed678c9198f
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-10 12:37:51 +0300

    Update ShareItApp.java

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,14 +3,11 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class ShareItApp {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ShareItApp.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
 		app.run(args);
 	}
 }
--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
 import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.model.User;
 
 public class BookingMapper {
 

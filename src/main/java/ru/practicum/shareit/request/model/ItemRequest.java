e4f1dc804b6fa0ce3202ef4a66b26bc3f72f01d0
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-11 16:41:17 +0300

    create : ItemRequest.java

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,4 +1,33 @@
 package ru.practicum.shareit.request.model;
 
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "requests")
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "created")
+    private LocalDateTime created;
 }

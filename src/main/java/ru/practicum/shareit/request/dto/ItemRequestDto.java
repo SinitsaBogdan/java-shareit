4ba178a6f9ecdd76673f4c9fd31acaff69e23031
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 17:25:26 +0300

    update : ItemRequestDto.java

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -4,8 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ItemRequestDto {
+
+    @NotBlank
+    private String description;
 }

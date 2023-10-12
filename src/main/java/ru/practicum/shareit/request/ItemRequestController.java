7845a2b5b9fa0096eb31c4d63828538ececdfd4e
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-13 00:00:25 +0300

    update : ItemRequestController.java

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit.request;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
+import ru.practicum.shareit.util.exeptions.BusinessException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -30,9 +32,14 @@ public class ItemRequestController {
      * Запрос на получение всех запросов вещей
      **/
     @GetMapping("/all")
-    public List<ItemRequestDto> getAll(@RequestHeader(value = "X-Sharer-User-Id") long userId, @RequestParam(defaultValue = "0") int from, @RequestParam(defaultValue = "10") int size) {
+    public List<ItemRequestDto> getAll(
+            @RequestHeader(value = "X-Sharer-User-Id") long userId,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size
+    ) {
         log.info("   GET [http://localhost:8080/requests/all] : Запрос на получение всех запросов вещей");
-        return itemRequestService.findAll(userId, from, size);
+        if (from < 0 || size < 1) throw new BusinessException("Некорректные параметры поиска", 400);
+        return itemRequestService.findAll(userId, PageRequest.of(from > 0 ? from / size : 0, size));
     }
 
     /**

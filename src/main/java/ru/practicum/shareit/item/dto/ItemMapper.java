80801f35a4c380aa3631ce05374f58af1cc0a545
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 20:06:34 +0300

    update : ItemMapper.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -24,6 +24,7 @@ public class ItemMapper {
                 .description(item.getDescription())
                 .available(item.getAvailable())
                 .comments(list)
+                .requestId(item.getRequest() != null ? item.getRequest().getId() : null)
                 .build();
     }
 

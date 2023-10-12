72ca7ca83d892d84befaccb0326f016c92e44488
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 20:05:56 +0300

    update : ItemRequestMapper.java

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
@@ -1,15 +1,23 @@
 package ru.practicum.shareit.request.dto;
 
 import org.jetbrains.annotations.NotNull;
+import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.stream.Collectors;
 
 public class ItemRequestMapper {
 
-    public static @NotNull ItemRequestDto mapperItemRequestToDto(@NotNull ItemRequest itemRequest) {
-        return ItemRequestDto.builder().description(itemRequest.getText()).build();
+    public static ItemRequestDto mapperItemRequestToDto(@NotNull ItemRequest itemRequest) {
+        return ItemRequestDto.builder()
+                .id(itemRequest.getId())
+                .description(itemRequest.getDescription())
+                .created(itemRequest.getCreated())
+                .items(itemRequest.getItems().stream().map(ItemMapper::mapperItemToDto).collect(Collectors.toList()))
+                .build();
     }
 
-    public static @NotNull ItemRequest mapperItemRequestDtoToItemRequest(@NotNull ItemRequestDto itemRequestDto) {
-        return ItemRequest.builder().text(itemRequestDto.getDescription()).build();
+    public static ItemRequest mapperItemRequestDtoToItemRequest(@NotNull ItemRequestDto itemRequestDto) {
+        return ItemRequest.builder().description(itemRequestDto.getDescription()).build();
     }
 }

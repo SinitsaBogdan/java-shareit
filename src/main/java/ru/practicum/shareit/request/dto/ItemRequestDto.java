696097f935444b38b19444a07b8a65c4c1574b2a
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 20:06:06 +0300

    update : ItemRequestDto.java

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,16 +1,31 @@
 package ru.practicum.shareit.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ItemRequestDto {
 
+    @Positive
+    private Long id;
+
     @NotBlank
     private String description;
+
+    private LocalDateTime created;
+
+    private List<ItemDto> items;
 }

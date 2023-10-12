5a15dccaab0e550f447bb06cabf3511e4f161cf3
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 17:24:49 +0300

    create : ItemRequestService.java

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,4 +1,16 @@
 package ru.practicum.shareit.request.service;
 
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
+
 public interface ItemRequestService {
+
+    List<ItemRequestDto> findAll(Long userId);
+
+    List<ItemRequestDto> findAll(long userId, int from, int size);
+
+    ItemRequestDto findOne(long userId, long requestId);
+
+    ItemRequestDto add(long userId, ItemRequestDto requestDto);
 }

23a96a8d07c896ce43f84813ab9382be59f463ad
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-12 20:05:35 +0300

    update : ItemRequest.java

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,10 +1,15 @@
 package ru.practicum.shareit.request.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Getter
@@ -25,9 +30,14 @@ public class ItemRequest {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(name = "text")
-    private String text;
+    @Column(name = "description")
+    private String description;
 
     @Column(name = "created")
     private LocalDateTime created;
+
+    @ToString.Exclude
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "request")
+    private final List<Item> items = new ArrayList<>();
 }

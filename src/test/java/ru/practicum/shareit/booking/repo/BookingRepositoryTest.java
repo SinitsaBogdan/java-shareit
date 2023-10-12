554f991637ee2c5e16b882feebbc756e203a37e9
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-13 01:34:17 +0300

    update : BookingControllerTest.java

--- src/test/java/ru/practicum/shareit/booking/repo/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/repo/BookingRepositoryTest.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.booking.repo;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
@@ -16,7 +14,6 @@ import java.time.LocalDateTime;
 
 
 @DataJpaTest
-@RunWith(SpringRunner.class)
 public class BookingRepositoryTest {
 
     @Autowired

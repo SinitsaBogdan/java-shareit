7ccf3f7e2989e86cfe3a558a7c612b13b946e603
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-13 23:56:14 +0300

    update : CommentRepositoryTest.java

--- src/test/java/ru/practicum/shareit/item/repo/CommentRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repo/CommentRepositoryTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import ru.practicum.shareit.item.model.Comment;
-import ru.practicum.shareit.user.model.User;
 
+import javax.validation.ValidationException;
 import java.time.LocalDateTime;
 
 @DataJpaTest
@@ -28,22 +29,41 @@ class CommentRepositoryTest {
     @DisplayName("ENTITY")
     public class Entity {
 
-        @Test
-        @DisplayName("Успешное сохранение объекта в таблицу COMMENTS")
-        public void save() {
-            Comment comment = Comment.builder()
-                    .text("text")
-                    .created(LocalDateTime.now())
-                    .build();
 
-            Assertions.assertNull(comment.getId());
-            commentRepository.save(comment);
-            Assertions.assertNotNull(comment.getId());
+
+        @Test
+        @DisplayName("Проверка валидации поля text - @CustomValidNotBlank")
+        public void save__Fail_Text_CustomValidNotBlank() {
+            Comment comment = Comment.builder().text("").created(LocalDateTime.now()).build();
+            Assertions.assertThrows(ValidationException.class, () -> commentRepository.save(comment));
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля text - nullable")
+        public void save__Fail_Text_nullable() {
+            Comment comment = Comment.builder().created(LocalDateTime.now()).build();
+            Assertions.assertThrows(ValidationException.class, () -> commentRepository.save(comment));
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля created - nullable")
+        public void save__Fail_Created_nullable() {
+            Comment comment = Comment.builder().text("text").build();
+            Assertions.assertThrows(DataIntegrityViolationException.class, () -> commentRepository.save(comment));
         }
     }
 
     @Nested
     @DisplayName("REPOSITORY")
     public class Repository {
+
+        @Test
+        @DisplayName("Успешное сохранение объекта в таблицу COMMENTS")
+        public void save() {
+            Comment comment = Comment.builder().text("text").created(LocalDateTime.now()).build();
+            Assertions.assertNull(comment.getId());
+            commentRepository.save(comment);
+            Assertions.assertNotNull(comment.getId());
+        }
     }
 }

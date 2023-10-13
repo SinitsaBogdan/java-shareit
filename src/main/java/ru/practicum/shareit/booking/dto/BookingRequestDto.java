9384570904fc6772d1d89f953ad46fa6cb78070d
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-13 17:33:21 +0300

    update : BookingRequestDto.java

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -3,6 +3,8 @@ package ru.practicum.shareit.booking.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Setter
+@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingRequestDto {
 

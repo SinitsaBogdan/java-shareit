0e03ebb5eaa4b4189ed6de02942f94a25dc3f9e7
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-13 20:46:37 +0300

    update : UserRepositoryTest.java

--- src/test/java/ru/practicum/shareit/user/repo/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/repo/UserRepositoryTest.java
@@ -2,49 +2,135 @@ package ru.practicum.shareit.user.repo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repo.ItemRepository;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.util.exeptions.CustomException;
 
+import javax.validation.ValidationException;
 
 @DataJpaTest
+@DisplayName("Интеграционное тестирование - UserRepository")
 class UserRepositoryTest {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Test
+    @DisplayName("CONFIG")
     public void contextLoads() {
-        Assertions.assertNotNull(repository);
+        Assertions.assertNotNull(userRepository);
     }
 
-    @Test
-    @DisplayName("Успешное сохранение объекта в таблицу USERS")
-    public void saveUser() {
-        User emp = User.builder()
-                .name("user-1")
-                .email("mail@mail.ru")
-                .build();
+    @Nested
+    @DisplayName("ENTITY")
+    public class Entity {
 
-        Assertions.assertNull(emp.getId());
-        repository.save(emp);
-        Assertions.assertNotNull(emp.getId());
+        @Test
+        @DisplayName("Успешное сохранение объекта в таблицу USERS")
+        public void save() {
+            User user = User.builder()
+                    .name("user-1")
+                    .email("mail@mail.ru")
+                    .build();
+
+            Assertions.assertNull(user.getId());
+            userRepository.save(user);
+            Assertions.assertNotNull(user.getId());
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля name - MaxLength20")
+        public void save__Fail_Name_MaxLength20() {
+            User user = User.builder()
+                    .name("user-1-BigName-BigName-BigName-33")
+                    .email("mail@mail.ru")
+                    .build();
+
+            Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+                userRepository.save(user);
+            });
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля name - @CustomValidNotBlank")
+        public void save__Fail_Name_CustomValidNotBlank() {
+            User user = User.builder()
+                    .name("")
+                    .email("mail@mail.ru")
+                    .build();
+
+            Assertions.assertThrows(ValidationException.class, () -> {
+                userRepository.save(user);
+            });
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля email - MaxLength36")
+        public void save__Fail_Name_MaxLength36() {
+            User user = User.builder()
+                    .name("user")
+                    .email("mail_BigName_BigName_BigName_BigName@mail.ru")
+                    .build();
+
+            Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+                userRepository.save(user);
+            });
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля email - @CustomValidEmail [ v1 ]")
+        public void save__Fail_Name_CustomValidEmail_v1() {
+            User user = User.builder()
+                    .name("user")
+                    .email("mail.mail.ru")
+                    .build();
+
+            Assertions.assertThrows(ValidationException.class, () -> {
+                userRepository.save(user);
+            });
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля email - @CustomValidEmail [ v2 ]")
+        public void save__Fail_Name_CustomValidEmail_v2() {
+            User user = User.builder()
+                    .name("user")
+                    .email("mail@mail_ru")
+                    .build();
+
+            Assertions.assertThrows(ValidationException.class, () -> {
+                userRepository.save(user);
+            });
+        }
+
+        @Test
+        @DisplayName("Проверка валидации поля email - unique")
+        public void save__Fail_Name_Unique() {
+            User user = User.builder()
+                    .name("user")
+                    .email("mail@mail.ru")
+                    .build();
+
+            userRepository.save(user);
+
+            User user_unique = User.builder()
+                    .name("user")
+                    .email("mail@mail.ru")
+                    .build();
+
+            Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+                userRepository.save(user_unique);
+            });
+        }
     }
 
-    @Test
-    @DisplayName("Проверка валидации поля name - MaxLength20")
-    public void saveUser__Fail_Name_MaxLength20() {
-        User emp = User.builder()
-                .name("user-1-BigName-BigName-BigName-33")
-                .email("mail@mail.ru")
-                .build();
-
-        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
-            repository.save(emp);
-        });
+    @Nested
+    @DisplayName("REPOSITORY")
+    public class Repository {
     }
-
 }

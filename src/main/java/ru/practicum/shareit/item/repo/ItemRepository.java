0931d16c86214aeea5953ab0e7ecfe178642995a
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-14 14:57:39 +0300

    update : ItemRepository.java

--- src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repo/ItemRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    List<Item> findByUser_id(long userId);
+    List<Item> findByUser(User user);
 
     @Query("select i from Item i where i.available <> false and (lower(i.name) LIKE lower(concat('%',:text,'%')) or lower(i.description) LIKE lower(concat('%',:text,'%')))")
     List<Item> findSearch(@Param("text") String text);

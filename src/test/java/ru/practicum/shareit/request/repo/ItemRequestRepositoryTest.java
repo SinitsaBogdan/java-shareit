858d31233303bf065e44c8f5e76cb722e44b09b1
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-10-14 14:56:06 +0300

    update : ItemRequestRepositoryTest.java

--- src/test/java/ru/practicum/shareit/request/repo/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/repo/ItemRequestRepositoryTest.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.request.repo;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -24,6 +21,28 @@ class ItemRequestRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private final User user_1 = User.builder()
+            .name("user-1")
+            .email("mail-1@mail.ru")
+            .build();
+
+    private final User user_2 = User.builder()
+            .name("user-2")
+            .email("mail-2@mail.ru")
+            .build();
+
+    private final ItemRequest request_1 = ItemRequest.builder()
+            .description("description-1")
+            .created(LocalDateTime.now())
+            .user(user_1)
+            .build();
+
+    private final ItemRequest request_2 = ItemRequest.builder()
+            .description("description-2")
+            .created(LocalDateTime.now())
+            .user(user_2)
+            .build();
+
     @Test
     @DisplayName("CONFIG")
     public void contextLoads() {
@@ -37,14 +56,9 @@ class ItemRequestRepositoryTest {
         @Test
         @DisplayName("Проверка валидации поля description - nullable")
         public void save__Fail_Description_nullable() {
-            User user = User.builder()
-                    .name("user-1")
-                    .email("mail-1@mail.ru")
-                    .build();
-
             ItemRequest request = ItemRequest.builder()
                     .created(LocalDateTime.now())
-                    .user(user)
+                    .user(user_1)
                     .build();
 
             Assertions.assertThrows(DataIntegrityViolationException.class, () -> requestRepository.save(request));
@@ -53,14 +67,9 @@ class ItemRequestRepositoryTest {
         @Test
         @DisplayName("Проверка валидации поля description - nullable")
         public void save__Fail_Name_nullable() {
-            User user = User.builder()
-                    .name("user-1")
-                    .email("mail-1@mail.ru")
-                    .build();
-
             ItemRequest request = ItemRequest.builder()
                     .description("description")
-                    .user(user)
+                    .user(user_1)
                     .build();
 
             Assertions.assertThrows(DataIntegrityViolationException.class, () -> requestRepository.save(request));
@@ -71,87 +80,35 @@ class ItemRequestRepositoryTest {
     @DisplayName("REPOSITORY")
     public class Repository {
 
+        @BeforeEach
+        public void beforeEach() {
+            userRepository.save(user_1);
+            userRepository.save(user_2);
+            Assertions.assertNull(request_1.getId());
+            Assertions.assertNull(request_2.getId());
+            requestRepository.save(request_1);
+            requestRepository.save(request_2);
+        }
+
         @Test
         @DisplayName("Успешное сохранение объекта в таблицу REQUESTS")
         public void save() {
-            User user = User.builder()
-                    .name("user-1")
-                    .email("mail-1@mail.ru")
-                    .build();
-
-            ItemRequest request = ItemRequest.builder()
-                    .description("description")
-                    .created(LocalDateTime.now())
-                    .user(user)
-                    .build();
-
-            Assertions.assertNull(request.getId());
-            requestRepository.save(request);
-            Assertions.assertNotNull(request.getId());
+            Assertions.assertNotNull(request_1.getId());
         }
 
         @Test
-        @DisplayName("Проверка метода - findByUser(User user)")
+        @DisplayName("Проверка метода - findByUser")
         public void findByUser_id() {
-
-            User user_1 = User.builder()
-                    .name("user-1")
-                    .email("mail-1@mail.ru")
-                    .build();
-
-            User user_2 = User.builder()
-                    .name("user-2")
-                    .email("mail-2@mail.ru")
-                    .build();
-
-            ItemRequest request = ItemRequest.builder()
-                    .description("description")
-                    .created(LocalDateTime.now())
-                    .user(user_2)
-                    .build();
-
-            userRepository.save(user_1);
-            userRepository.save(user_2);
-            requestRepository.save(request);
-
             List<ItemRequest> result = requestRepository.findByUser(user_2);
             Assertions.assertEquals(result.size(), 1);
         }
 
         @Test
-        @DisplayName("Проверка метода - findByUser(User user)")
+        @DisplayName("Проверка метода - findByUser")
         public void findByUser() {
-
-            User user_1 = User.builder()
-                    .name("user-1")
-                    .email("mail-1@mail.ru")
-                    .build();
-
-            User user_2 = User.builder()
-                    .name("user-2")
-                    .email("mail-2@mail.ru")
-                    .build();
-
-            ItemRequest request_1 = ItemRequest.builder()
-                    .description("description-1")
-                    .created(LocalDateTime.now())
-                    .user(user_1)
-                    .build();
-
-            ItemRequest request_2 = ItemRequest.builder()
-                    .description("description-2")
-                    .created(LocalDateTime.now())
-                    .user(user_2)
-                    .build();
-
-            userRepository.save(user_1);
-            userRepository.save(user_2);
-            requestRepository.save(request_1);
-            requestRepository.save(request_2);
-
             List<ItemRequest> result = requestRepository.findByUser(user_2);
             Assertions.assertEquals(result.size(), 1);
-            Assertions.assertEquals(result.get(0).getDescription(), "description-1");
+            Assertions.assertEquals(result.get(0).getDescription(), "description-2");
         }
     }
 }

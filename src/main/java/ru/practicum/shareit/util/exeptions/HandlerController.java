c2463a5e301b18d7ef7e4abdf78ca5f7dba7b453
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-10-15 15:58:18 +0300

    Update HandlerController.java

--- src/main/java/ru/practicum/shareit/util/exeptions/HandlerController.java
+++ src/main/java/ru/practicum/shareit/util/exeptions/HandlerController.java
@@ -8,8 +8,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class HandlerController {
 
-    @ExceptionHandler({BusinessException.class, ServiceException.class, RepositoryException.class, CustomException.class})
+    @ExceptionHandler(BusinessException.class)
     public ResponseEntity<Response> handleException(@NotNull BusinessException exception) {
+        Response response = new Response(exception.getMessage(), null);
+        return new ResponseEntity<>(response, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<Response> handleException(@NotNull ServiceException exception) {
+        Response response = new Response(exception.getMessage(), null);
+        return new ResponseEntity<>(response, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler(RepositoryException.class)
+    public ResponseEntity<Response> handleException(@NotNull RepositoryException exception) {
+        Response response = new Response(exception.getMessage(), null);
+        return new ResponseEntity<>(response, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Response> handleException(@NotNull CustomException exception) {
         Response response = new Response(exception.getMessage(), null);
         return new ResponseEntity<>(response, exception.getHttpStatus());
     }
